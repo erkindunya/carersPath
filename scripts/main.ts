10b99725968590064f0e0f65b60ca3271a387403
Joe Flitcroft <flitcroft56@gmail.com>
2018-09-28 16:44:16 +0100

    Job role landing complete, job role started

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,7 +1,12 @@
 import $ from 'jquery';
 
 $(document).on("click", ".js-toggle-subnav", () => {
-    $(".subnav-link").toggleClass("active");
-    $(".subnav").slideToggle(200);
-  });
-  
+  $(".subnav-link").toggleClass("active");
+  $(".subnav").slideToggle(200);
+});
+
+$(document).on("click", "#exploreCareer .arrow", () => {
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $("#exploreCareer").offset().top
+  }, 1000);
+});
---
+++ scripts/scrollpane.js
@@ -0,0 +1,73 @@
+
+var $ = document.getElementById.bind(document),
+  container = $('scrollbar-container'),
+  content = $('scrollbar-content'),
+  scroll = $('scrollbar'),
+  scrollLower = $('scrollbar-lower');
+
+content.addEventListener('scroll', function (e) {
+
+  // get scrollable width (the amount hanging off the edge of the screen)
+  var scrollableWidth = content.scrollWidth - content.clientWidth;
+  // get percentage content scrolled
+  var percentScrolled = 100 / (scrollableWidth / content.scrollLeft);
+  //apply percentage to remaining scrollable width and set scrollLeft to result
+  scroll.style.left = ((container.clientWidth - scroll.clientWidth) / 100) * percentScrolled + "px";
+  scrollLower.style.left = ((container.clientWidth - scrollLower.clientWidth) / 100) * percentScrolled + "px";
+});
+
+var event = new Event('scroll');
+
+window.addEventListener('resize', content.dispatchEvent.bind(content, event));
+content.dispatchEvent(event);
+
+
+function addScrollbarEvent(scrollbar) {
+  scrollbar.addEventListener('mousedown', function (start) {
+    start.preventDefault();
+    var y = scrollbar.offsetLeft;
+    var onMove = function (end) {
+      var delta = end.pageX - start.pageX;
+      scrollbar.style.left = Math.min(container.clientWidth - scrollbar.clientWidth, Math.max(0, y + delta)) + 'px';
+
+      // percent of scrollbar moved
+      var percentScrolled = 100 / ((container.clientWidth - scrollbar.clientWidth) / scrollbar.offsetLeft);
+      content.scrollLeft = ((content.scrollWidth - content.clientWidth) / 100) * percentScrolled;
+    };
+    console.log(start);
+    document.addEventListener('mousemove', onMove);
+    document.addEventListener('mouseup', function () {
+      document.removeEventListener('mousemove', onMove);
+    });
+  });
+}
+
+
+// touchscreens
+function addScrollbarTouchEvent(scrollbar) {
+  scrollbar.addEventListener('touchstart', function (start) {
+    start.preventDefault();
+    var y = scrollbar.offsetLeft;
+    var onMove = function (end) {
+      var delta = end.touches[0].pageX - start.touches[0].pageX;
+      scrollbar.style.left = Math.min(container.clientWidth - scrollbar.clientWidth, Math.max(0, y + delta)) + 'px';
+
+      // percent of scrollbar moved
+      var percentScrolled = 100 / ((container.clientWidth - scrollbar.clientWidth) / scrollbar.offsetLeft);
+      content.scrollLeft = ((content.scrollWidth - content.clientWidth) / 100) * percentScrolled;
+    };
+    console.log(start);
+    document.addEventListener('touchmove', onMove);
+    document.addEventListener('touchend', function () {
+      document.removeEventListener('touchmove', onMove);
+    });
+  });
+}
+
+//desktop
+addScrollbarEvent(scroll);
+addScrollbarEvent(scrollLower);
+
+// mobile
+addScrollbarTouchEvent(scroll);
+addScrollbarTouchEvent(scrollLower);
